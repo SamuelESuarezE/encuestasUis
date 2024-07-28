2e6d10aa65fc6dbbf711253c308d89b0d8f28ab7
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-07-28 13:37:02 -0500

    feat: :tada: Commit inicial para proyecto encuestasUis

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+
+</script>
+
+<template>
+    <h1>Hello world</h1>
+</template>
+
+<style scoped>
+</style>
+
---
+++ src/views/AboutView.vue
@@ -0,0 +1,3 @@
+<template>
+    <h1>RUTA</h1>
+</template>
