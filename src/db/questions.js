2dc35ecbd18d5c699d75e512eb7da9a72dd1b694
SamuelESuarezE <151666245+SamuelESuarezE@users.noreply.github.com>
2024-08-03 08:44:58 -0500

    fix: :alien: Se actualizo url de api de testing

--- src/db/questions.js
+++ src/db/questions.js
@@ -1,6 +1,6 @@
 export const getQuestionsBySurveyAndChapter = async(surveyId, chapterId) => {
     try {
-        const response = await fetch(`http://localhost:5501/questions?chapter_id=${chapterId}&survey_id=${surveyId}`);
+        const response = await fetch(`https://apiencuestas.onrender.com/questions?chapter_id=${chapterId}&survey_id=${surveyId}`);
         const questions = await response.json();
         return questions;
     } catch (error) {
